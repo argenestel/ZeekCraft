3824c077f67df85239a4e1d52ac7c6e1970d07a3
argenestel <arpitpali68@gmail.com>
2024-07-17 11:26:19 +0530

    ZeekCraft game

---
+++ game/src/constants/addresses.ts
@@ -0,0 +1,5 @@
+export const addresses = {
+  // ZetaTokenForTest: "0x4b3DeA2232fB8E5419CfacF9f633bf638267699a",
+  ZeekCraft: "0xF9dE10bCB84901b67a19d99F535989521B8d03FB",
+  ZeekCraftToken: "0x794CdABc17830f4bcaAACa7ee1658c31942845dA",
+};
